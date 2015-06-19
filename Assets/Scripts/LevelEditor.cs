c986742db5710628fe27a3ecc1d411f8bb092d2f
Rodrigo Rodrigues do Carmo <rodrigorcarmo87@gmail.com>
2015-06-19 13:54:58 -0500

    Wrap text on images

--- Assets/Scripts/LevelEditor.cs
+++ Assets/Scripts/LevelEditor.cs
@@ -322,6 +322,43 @@ public class LevelEditor : MonoBehaviour {
         GameObject textObject = text.gameObject;
         child.GetComponent<SpriteRenderer>().sprite = Resources.Load <Sprite>("Images/Text");
         textObject.GetComponent<TextMesh>().fontSize = 45;
-        textObject.GetComponent<TextMesh>().text = phrase;
+        textObject.GetComponent<TextMesh>().text = ResolveTextSize(phrase,15);
+    }
+
+    private string ResolveTextSize(string input, int lineLength){
+        
+        // Split string by char " "         
+        string[] words = input.Split(" "[0]);
+        
+        // Prepare result
+        string result = "";
+        
+        // Temp line string
+        string line = "";
+        
+        // for each all words        
+        foreach(string s in words){
+            // Append current word into line
+            string temp = line + " " + s;
+            
+            // If line length is bigger than lineLength
+            if(temp.Length > lineLength){
+                
+                // Append current line into result
+                result += line + "\n";
+                // Remain word append into new line
+                line = s;
+            }
+            // Append current word into current line
+            else {
+                line = temp;
+            }
+        }
+        
+        // Append last line into result        
+        result += line;
+        
+        // Remove first " " char
+        return result.Substring(1,result.Length-1);
     }
 }
